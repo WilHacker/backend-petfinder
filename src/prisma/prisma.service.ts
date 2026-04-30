2f3c5db1334b8f00a37c7259feffe1bd29020fe0
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2026-04-30 02:01:20 -0400

    Fix(Prisma.service): se arrelgo la parte de slint

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,31 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    });
-  }
-
   async onModuleInit() {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      await this.$connect();
-      // eslint-disable-next-line no-console
-      console.log('✅ Conexión establecida con Prisma 7 y Supabase');
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('❌ Error al conectar a la base de datos:', error);
-    }
+    await this.$connect();
+    // eslint-disable-next-line no-console
+    console.info('✅ Conexión exitosa a Supabase (PetFinder DB)');
   }
 
   async onModuleDestroy() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
   }
 }
