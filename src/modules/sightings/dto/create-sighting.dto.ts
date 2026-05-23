aba5915b599261e58dfbc7951be1d888dcc9d4a2
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2026-05-22 22:50:57 -0400

    feat(sightings): agregar transformación para latitud y longitud en el DTO de avistamientos

--- src/modules/sightings/dto/create-sighting.dto.ts
+++ src/modules/sightings/dto/create-sighting.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional, IsString, Max, MaxLength, Min } from 'class-validator';
 
 export class CreateSightingDto {
   @ApiProperty({ example: -17.3935, description: 'Latitud del avistamiento' })
+  @Transform(({ value }) => parseFloat(value as string))
   @IsNumber()
   @Min(-90)
   @Max(90)
   lat!: number;
 
   @ApiProperty({ example: -66.157, description: 'Longitud del avistamiento' })
+  @Transform(({ value }) => parseFloat(value as string))
   @IsNumber()
   @Min(-180)
   @Max(180)
